e66d2a2bea2ea7b04d18bbfd267ef4207ad4cc3f
Marwa <koroko123430@gmail.com>
2022-07-03 10:50:21 +0300

    design improvement

--- src/app/components/main/chat-list/chat-list.component.ts
+++ src/app/components/main/chat-list/chat-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IChat } from 'src/app/interfaces/chat';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ChatService } from 'src/app/services/chat/chat.service';
@@ -9,23 +9,28 @@ import { Subscription } from 'rxjs';
   templateUrl: './chat-list.component.html',
   styleUrls: ['./chat-list.component.css']
 })
-export class ChatListComponent implements OnInit {
+export class ChatListComponent implements OnInit, OnDestroy {
   chats!: IChat[]
   subscribtion!: Subscription;
 
 
   constructor(private _chatService: ChatService, private _authService: AuthService) {
-    this.subscribtion=this._chatService.listChange.subscribe(chatList =>{this.chats = chatList;
-    console.log("hi from chat list")
-    })//عشان يتغير مع تغير القيمة في السيرفيس
+    this.subscribtion = this._chatService.listChange.subscribe(chatList => {
+      this.chats = chatList;
+    })
 
     this._chatService.getUserChatList().subscribe(
       (chatList) => {
+
         this.chats = this.deleteAuthUser(chatList)
+        console.log(this.chats);
       }
     );
 
 
+  }
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
   }
 
   ngOnInit(): void {
