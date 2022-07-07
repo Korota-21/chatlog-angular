a7a29ba436c23cd1c5e577706ec59f1022760aad
Marwa <koroko123430@gmail.com>
2022-07-07 10:26:05 +0300

    update user interface to have user object

--- src/app/interfaces/message.ts
+++ src/app/interfaces/message.ts
@@ -4,7 +4,7 @@ import { IUser } from "./user";
 export interface IMessage {
   _id: string,
   message: string,
-  user: string,
+  user: IUser,
   chat: IChat,
   updatedAt: string,
   createdAt: string,
